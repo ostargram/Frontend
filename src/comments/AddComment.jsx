759f9201acb8c96ead0c75188f92cd1c66b0d5aa
cheongu <kangsun0528@gmail.com>
2023-01-17 23:44:29 +0900

    comments add

---
+++ src/comments/AddComment.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+// import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { __addComment } from "../redux/modules/commentsSlice";
+
+const AddComment = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const [comment, setComment] = useState({
+    content: "",
+  });
+
+  const onAddCommentButtonHandler = (event) => {
+    event.preventDefault();
+    if (comment.content.trim() === "") {
+      return alert("모든 항목을 입력해주세요.");
+    }
+    dispatch(__addComment({ todoId: id, ...comment }));
+    setComment({
+      content: "",
+    });
+  };
+
+  const onChangeInputHandler = (event) => {
+    const { name, value } = event.target;
+    setComment({
+      ...comment,
+      [name]: value,
+    });
+  };
+
+  return (
+    <form onSubmit={onAddCommentButtonHandler}>
+      <input
+        placeholder="댓글을 추가하세요. (100자 이내)"
+        value={comment.content}
+        name="content"
+        type="text"
+        onChange={onChangeInputHandler}
+        maxLength={100}
+      />
+      <button type="submit" onClick={onAddCommentButtonHandler}>
+        추가하기
+      </button>
+    </form>
+  );
+};
+
+export default AddComment;
---
+++ src/comments/Comment.jsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+// import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  __deleteComment,
+  __updateComment,
+} from "../redux/modules/commentsSlice";
+import {
+  clearComment,
+  globalEditModeToggle,
+  __getComment,
+} from "../redux/modules/commentSlice";
+
+const Comment = ({ comment }) => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const [isEdit, setIsEdit] = useState(false);
+  const [updatedComment, setUpdatedComment] = useState("");
+
+  const { content } = useSelector((state) => state.comment.data);
+  const { isGlobalEditmode } = useSelector((state) => state.comment);
+
+  const onDeleteButtonHandler = () => {
+    const result = window.confirm("삭제하시겠습니까?");
+    if (result) {
+      dispatch(__deleteComment(comment.id));
+    } else {
+      return;
+    }
+  };
+
+  const onUpdateButtonHandler = () => {
+    dispatch(
+      __updateComment({
+        id: comment.id,
+        content: updatedComment,
+
+        todoId: id,
+      })
+    );
+    setIsEdit(false);
+    dispatch(globalEditModeToggle(false));
+  };
+
+  const onChangeEditButtonHandler = () => {
+    setIsEdit(true);
+    dispatch(__getComment(comment.id));
+    dispatch(globalEditModeToggle(true));
+  };
+
+  const onCancelButtonHandler = () => {
+    setIsEdit(false);
+    dispatch(clearComment());
+    dispatch(globalEditModeToggle(false));
+  };
+
+  useEffect(() => {
+    setUpdatedComment(content);
+  }, [content]);
+
+  return (
+    <div>
+      {isEdit ? (
+        <>
+          <div>
+            <input
+              type="text"
+              value={updatedComment}
+              maxlength={100}
+              onChange={(event) => {
+                setUpdatedComment(event.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <button onClick={onCancelButtonHandler}>
+              <h5>취소</h5>
+            </button>
+            <button onClick={onUpdateButtonHandler}>
+              <h5>저장</h5>
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <div>{comment.username}</div>
+            <div>{comment.content}</div>
+          </div>
+
+          <div>
+            <button
+              disabled={isGlobalEditmode}
+              onClick={onChangeEditButtonHandler}
+            ></button>
+            <button
+              onClick={onDeleteButtonHandler}
+              disabled={isGlobalEditmode}
+            ></button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Comment;
